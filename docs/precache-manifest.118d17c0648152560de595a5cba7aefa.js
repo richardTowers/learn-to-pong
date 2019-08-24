daa5a4ca856ba3d73bc2506465a6f5c8b5ca255b
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2019-08-24 22:00:19 +0100

    Add docs folder for github pages
    Also set "homepage" in package.json so the files will be served from the
    right folder.

---
+++ docs/precache-manifest.118d17c0648152560de595a5cba7aefa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a13067baefa4af728819aec946f67a54",
+    "url": "/learn-to-pong/index.html"
+  },
+  {
+    "revision": "9365d79022751c73fe8f",
+    "url": "/learn-to-pong/static/css/2.66259513.chunk.css"
+  },
+  {
+    "revision": "109f389635aa2747f470",
+    "url": "/learn-to-pong/static/css/main.e30fda37.chunk.css"
+  },
+  {
+    "revision": "9365d79022751c73fe8f",
+    "url": "/learn-to-pong/static/js/2.7f0ed3ed.chunk.js"
+  },
+  {
+    "revision": "109f389635aa2747f470",
+    "url": "/learn-to-pong/static/js/main.8318a56c.chunk.js"
+  },
+  {
+    "revision": "0de6f3f6ad7c0dbbbb81",
+    "url": "/learn-to-pong/static/js/runtime~main.5b2ce8fb.js"
+  }
+]);
---
+++ docs/precache-manifest.c3b3a0d89a6cd0b4aff8849dd9e1b96a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da631b0bfd875476b2408ed9e71a160e",
+    "url": "index.html"
+  },
+  {
+    "revision": "9365d79022751c73fe8f",
+    "url": "static/css/2.66259513.chunk.css"
+  },
+  {
+    "revision": "ba8e70a98a703453bfc0",
+    "url": "static/css/main.e30fda37.chunk.css"
+  },
+  {
+    "revision": "9365d79022751c73fe8f",
+    "url": "static/js/2.7f0ed3ed.chunk.js"
+  },
+  {
+    "revision": "ba8e70a98a703453bfc0",
+    "url": "static/js/main.9d1c4ce6.chunk.js"
+  },
+  {
+    "revision": "7d16d17bc61dbd0b4a71",
+    "url": "static/js/runtime~main.6a6d4b14.js"
+  }
+]);
