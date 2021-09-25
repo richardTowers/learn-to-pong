e02db26a128db49871b2f093dd21cbaf26027492
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2021-09-25 17:59:07 +0100

    Rebuild docs

---
+++ docs/precache-manifest.7faad5e763ae899e525fc6313816e26f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f738343b59997f6bdce0c00b7b78b2eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3acad7011801b15192db",
+    "url": "/static/css/2.cbe49a2c.chunk.css"
+  },
+  {
+    "revision": "8eee1eacf1a35b88ab8f",
+    "url": "/static/css/main.d2541d77.chunk.css"
+  },
+  {
+    "revision": "3acad7011801b15192db",
+    "url": "/static/js/2.13e18b38.chunk.js"
+  },
+  {
+    "revision": "27859babfc8cc266977d9eb1a34fe96a",
+    "url": "/static/js/2.13e18b38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eee1eacf1a35b88ab8f",
+    "url": "/static/js/main.91b7c1cd.chunk.js"
+  },
+  {
+    "revision": "7466c156496f40ec532a",
+    "url": "/static/js/runtime-main.9b33e42e.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.55b4886871b9fda634ea128022b866d4.js"
+  "/precache-manifest.7faad5e763ae899e525fc6313816e26f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
